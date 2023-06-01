c18d492e60583254f2e84c33b37aba6be7fde548
ALIFIA PUTRI BUDIYANTI <asiyah_xav@student.ub.ac.id>
2023-06-01 13:20:21 +0700

    Splash screen dan on boarding

---
+++ app/src/main/java/com/example/e_library/layout_activity/OnBoarding.java
@@ -0,0 +1,16 @@
+package com.example.e_library.layout_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.e_library.R;
+
+public class OnBoarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
